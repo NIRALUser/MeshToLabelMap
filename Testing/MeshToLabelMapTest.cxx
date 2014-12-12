fa5244c48209b890f6315bb92a7347ac6ecacec7
fbudin69500 <fbudin@unc.edu>
2014-12-12 11:47:40 -0500

    ENH: If libraries are built statically, do not import dll on windows

--- Testing/MeshToLabelMapTest.cxx
+++ Testing/MeshToLabelMapTest.cxx
@@ -1,10 +1,14 @@
 #include "itkTestMain.h"
 
 #ifdef WIN32
-#define MODULE_IMPORT __declspec(dllimport)
-#else
-#define MODULE_IMPORT
-#endif
+#ifdef BUILD_SHARED_LIBS // variable declared in Cmake using "set_target_properties(... COMPILE_FLAGS -D...)"
+#define MODULE_IMPORT __declspec(dllimport) // "dllimport" -> Dll = Dynamic library on windows -> DTIAtlasBuilderLib is static, so just set MODULE_IMPORT to nothing: Fails to link (linkage error) if dllimport because library is static and tries to link as dynamic
+#else // BUILD_SHARED_LIBS
+#define MODULE_IMPORT// define to empty
+#endif // BUILD_SHARED_LIBS
+#else // WIN32
+#define MODULE_IMPORT// define to empty
+#endif // WIN32
 
 extern "C" MODULE_IMPORT int ModuleEntryPoint( int , char * [] ) ;
 void RegisterTests()
